d8238fd38f7338f1f2e461201803b2e982064bfa
Wube <wubshetkifle@wubshets-mbp.myfiosgateway.com>
2022-03-20 20:43:36 -0400

    formio uswds fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import './App.css'
-import 'uswds/dist/css/uswds.min.css';
-
-function App() {
+import React, { useState } from 'react';
+import { Form } from 'react-formio';
+import uswds from '@formio/uswds';
+import { Formio } from 'formiojs';
+import LoadingOverlay from 'react-loading-overlay';
+import { FadeLoader } from 'react-spinners';
+Formio.use(uswds);
+export const App = () => {
+  const [loader, setLoader] = useState(true);
+  const handelOnFormReady = () => {
+    setLoader(false);
+  };
   return (
-    <div className="text-bold" style={{ textAlign: 'center'}}>
-         OES A/B Testing
+    <div style={{margin: '5rem 15rem 1rem 15rem'}}>
+    <LoadingOverlay
+      active={loader}
+      styles={{ overlay: base => ({ ...base, background: 'rgba(0, 0, 0, 0.1)' }) }}
+      spinner={<FadeLoader color={'#4A90E2'} />}
+    >
+      <Form src="https://portal-test.forms.gov/oes-dev/oessurveyforma" onRender={handelOnFormReady} />
+    </LoadingOverlay>
     </div>
   );
-}
+};
 
 export default App;

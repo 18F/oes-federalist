5a4168b404ff98d5976731150dfa4d751396e2dc
ali <aalikaram@newwave.io>
2022-03-25 15:45:46 -0400

    formating done

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,6 @@
-import React from 'react'
-const Home = ()=> {
-  return (
- <div>
-     <button className="usa-button">Default</button>
-     Home
- </div>
-
-  );
-}
+import React from "react";
+const Home = () => {
+  return <div>Home</div>;
+};
 
 export default Home;

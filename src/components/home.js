5048c207635bfb23015bc78872cfcb14316a980b
ali <aalikaram@newwave.io>
2022-03-25 13:05:53 -0400

    routes are done

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Home = ()=> {
+  return (
+ <div>
+     <button className="usa-button">Default</button>
+     Home
+ </div>
+
+  );
+}
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Forms from './components/forms';
+import Home from './components/home';
+
+
+
+
+const Routing = () => {
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/site/18f/oes-federalist/" element={<Home />} />
+                <Route path="/site/18f/oes-federalist/form/version-a" element={<Forms />} />
+                <Route path="/site/18f/oes-federalist/form/version-b" element={<Forms />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Routing;
